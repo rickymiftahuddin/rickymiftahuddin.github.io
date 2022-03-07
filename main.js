1b0bf1c41fb4493e9187dca145f689ddb2acb1e2
Ricky Miftahuddin <64578906+rickymiftahuddin@users.noreply.github.com>
2022-03-07 14:27:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
+
+    let tax
+    let ptkp = 50000000
+
+
+    if (marriageStatus == "Menikah") {
+        ptkpChildCount = dependentChildrenCount*15000000
+        ptkp += ptkpChildCount+10000000
+    } else if (marriageStatus == "Cerai") {
+        if (dependentChildrenCount > 0) {
+            ptkpChildCount = dependentChildrenCount*15000000
+            ptkp += ptkpChildCount+20000000
+        }
+    }
+
+    const pkp = annualIncome-ptkp
+    if (pkp<=0) {
+        pkp=0
+    }
+
+    let tax1 = pkp*.1
+    let tax2 = (pkp-200000000)*.2
+    let tax3 = (pkp-200000000-250000000)*.3
+
+    if (tax1<=0) {
+        tax1 = 0
+    }
+    if (tax2<=0) {
+        tax2 = 0
+    }
+    if (tax3<=0) {
+        tax3 = 0
+    }
+
+    tax = tax1+tax2+tax3
+
+    console.log(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount)
+    return tax
+}
